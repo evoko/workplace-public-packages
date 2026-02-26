c228917a6466a734592026013cde6d0a299459f2
E-Joon Ko <e.joon.ko@umain.com>
2026-02-26 16:16:56 +0100

    Implemented canvas context

---
+++ packages/canvas/src/context/index.ts
@@ -0,0 +1,11 @@
+export { EditCanvasProvider, useEditCanvasContext } from './EditCanvasContext';
+export type {
+  EditCanvasProviderProps,
+  EditCanvasContextValue,
+} from './EditCanvasContext';
+
+export { ViewCanvasProvider, useViewCanvasContext } from './ViewCanvasContext';
+export type {
+  ViewCanvasProviderProps,
+  ViewCanvasContextValue,
+} from './ViewCanvasContext';
