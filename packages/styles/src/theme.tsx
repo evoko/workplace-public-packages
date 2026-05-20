9664a289df30dfbfa17d799d4522db66f56b1c91
simonhocker-umain <simon.hocker@umain.com>
2026-05-20 08:51:07 +0200

    Fix based on feedback

--- packages/styles/src/theme.tsx
+++ packages/styles/src/theme.tsx
@@ -2194,6 +2194,8 @@ export const biampTheme = (
               fontWeight: theme.typography.fontWeightMedium,
               letterSpacing: theme.typography.caption.letterSpacing,
               lineHeight: theme.typography.caption.lineHeight,
+              backgroundColor: theme.palette.background.default,
+              color: theme.palette.text.primary,
               '&.MuiBadge-dot': {
                 padding: '0px',
               },
