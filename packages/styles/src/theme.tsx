ce01fba0c39a1cb0d99cc7140fb14cba7e85167e
Ariño Miguel (Contractor) <miguel.arino@umain.com>
2026-06-15 15:08:50 +0200

    Overlay button fix.

--- packages/styles/src/theme.tsx
+++ packages/styles/src/theme.tsx
@@ -654,7 +654,7 @@ export const biampTheme = (
                 height: '48px !important',
                 borderRadius: '0px !important',
                 width: '100%',
-                flex: 1,
+                flexShrink: 1,
               },
             },
             {
